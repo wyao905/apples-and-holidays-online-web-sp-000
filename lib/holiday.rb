936d4efde97ca554b2020a8698547e70fa0e759a
Warren Yao <warrenyca905@gmail.com>
2019-05-23 05:57:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |seasons, holidays|
+    holidays.each do |holiday, supply|
+      if 
 end
 
 
